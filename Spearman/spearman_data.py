dfd02e7c84cc56f050b2e1800f54a066660ed9ca
pypas <pypasofar@gmail.com>
2020-12-05 22:18:57 -0300

    Updated Spearman graph

--- Spearman/spearman_data.py
+++ Spearman/spearman_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import spearmanr
 import streamlit as st
+import altair as alt
 
 path = "CSV/TabNet/Internacoes_Rate/"
 corrs_df = pd.read_csv("Spearman/correlation.csv",index_col=0)
@@ -21,12 +22,15 @@ def plot_disease_vs_suicide(disease):
     disease_df = disease_df[["MUNCOD", "Total"]]
     disease_df = disease_df[(disease_df["Total"] != 0)] # Exclude rows with 0 suicides
     final_df = pd.merge(disease_df, suicide_df, on="MUNCOD")
-    fig, ax = plt.subplots()
-    ax.scatter(final_df["Total"], final_df["SUICIDE"], s=5)
-    plt.title("Suicídios vs " + disease + " (2008-2018)")
-    plt.ylabel("Taxa de suicídios")
-    plt.xlabel("Taxa de Internações (" + disease + ")")
-    st.pyplot(fig, dpi=100)
+    base = alt.Chart(final_df, title="Suicídios vs " + disease + " (2008-2018)").mark_circle(color="black").encode(
+            x=alt.X('Total', title="Taxa de Internações (" + disease + ")"),
+            y=alt.Y("SUICIDE", title="Taxa de suicídios"),
+            tooltip=[alt.Tooltip('SUICIDE', title='Taxa de suicídios'), alt.Tooltip('Total', title='Taxa de Internações')]
+    )
+    regression_line = base.transform_regression(
+             "Total", "SUICIDE", method="poly", order=1).mark_line()
+
+    st.altair_chart(alt.layer(base, regression_line).properties(width=700, height=410))
 
 def get_diseases_select_names():
     global mapper
